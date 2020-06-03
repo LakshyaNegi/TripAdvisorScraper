f96eb12bf035316cb8ccd2826daa7bec1532f1e1
LakshyaNegi <lakshya.negi.1997@gmail.com>
2020-06-03 20:37:24 +0530

    scraper first commit

---
+++ hotel_spider/hotel_spider/spiders/hotels.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Request
+
+
+class HotelsSpider(scrapy.Spider):
+    name = 'hotels'
+    allowed_domains = ['tripadvisor.in']
+    start_urls = ['http://www.tripadvisor.in/Hotels-g304551-New_Delhi_National_Capital_Territory_of_Delhi-Hotels.html/']
+
+    def parse_reviews(self,response):
+        review_tabs = response.xpath('//div[@class="location-review-review-list-parts-SingleReview__mainCol--1hApa"]')
+
+        for review_tab in review_tabs:
+            bubbles = review_tab.xpath('div[@class="location-review-review-list-parts-RatingLine__container--2bjtw"]/div[@class="location-review-review-list-parts-RatingLine__bubbles--GcJvM"]/span/@class').extract_first()
+            review = review_tab.xpath('div[@class="location-review-review-list-parts-ExpandableReview__containerStyles--1G0AE"]//q/span/text()').extract_first()
+        
+        next_review_url = response.xpath('//div[@class="location-review-pagination-card-PaginationCard__wrapper--3epz_"]/div[@class="ui_pagination is-centered"]/a[@class="ui_button nav next primary "]/@href').extract_first()
+        absolute_next_url = response.urljoin(next_review_url)
+
+        yield Request(absolute_next_url, callback=self.parse_reviews)
+
+
+    
+    def parse_hotel(self,response):
+        header = response.xpath('//div[@id="taplc_hotel_review_atf_hotel_info_web_component_0"]')
+        name = header.xpath('//h1[@id="HEADING"]/text()').extract_first()
+        contact = header.xpath('//div[@class="hotels-hotel-review-atf-info-parts-ATFInfo__reverseActions--37dZ3"]')
+        contact = contact.xpath('//div[@class="hotels-hotel-review-atf-info-parts-BusinessListing__row--24M_7"]')
+        address_block = contact.xpath('//div[@class="hotels-hotel-review-atf-info-parts-BusinessListingEntry__entry--210S0 hotels-hotel-review-atf-info-parts-BusinessListingEntry__address--1Vy86"]')
+        address = address_block.xpath('//span[@class="public-business-listing-ContactInfo__nonWebLink--2rxPP public-business-listing-ContactInfo__ui_link_container--37q8W public-business-listing-ContactInfo__level_4--3JgmI"]/span/text()').extract_first()
+        phone = contact.xpath('//div[@class="hotels-hotel-review-atf-info-parts-BusinessListingEntry__entry--210S0 hotels-hotel-review-atf-info-parts-BusinessListingEntry__phone--1e9vv"]/div[@data-blcontact="PHONE "]/a/@href').extract_first()
+        #website = contact.xpath('//div[@class="hotels-hotel-review-atf-info-parts-BusinessListingEntry__entry--210S0 hotels-hotel-review-atf-info-parts-BusinessListingEntry__phone--1e9vv"]/div[@data-blcontact="URL_HOTEL "]/a/@class').extract_first()
+
+        review_div = response.xpath('//div[@class="ui_columns hotels-hotel-review-about-with-photos-layout-LayoutStrategy__columns--1uvt4"]')
+        review_div = review_div.xpath('//div[@class="ui_column  "]')
+        
+        left_review_div = review_div[0]
+        overall_rating = left_review_div.xpath('//div[@class="hotels-hotel-review-about-with-photos-Reviews__rating--2X_zZ hotels-hotel-review-about-with-photos-Reviews__cx_brand_refresh_phase2--3eimy"]/span/text()').extract_first()
+        left_review_div_rating_list = left_review_div.xpath('//div[@class="hotels-hotel-review-about-with-photos-Reviews__subratingRow--2u0CJ"]')
+        
+        
+        right_review_div = review_div[1]
+        amenities = right_review_div.xpath('div[@class="ssr-init-26f"]/@data-ssrev-handlers').extract_first()
+
+        review_tabs = response.xpath('//div[@class="location-review-review-list-parts-SingleReview__mainCol--1hApa"]')
+
+        for review_tab in review_tabs:
+            bubbles = review_tab.xpath('div[@class="location-review-review-list-parts-RatingLine__container--2bjtw"]/div[@class="location-review-review-list-parts-RatingLine__bubbles--GcJvM"]/span/@class').extract_first()
+            review = review_tab.xpath('div[@class="location-review-review-list-parts-ExpandableReview__containerStyles--1G0AE"]//q/span/text()').extract_first()
+        
+        
+        next_review_url = response.xpath('//div[@class="location-review-pagination-card-PaginationCard__wrapper--3epz_"]/div[@class="ui_pagination is-centered"]/a[@class="ui_button nav next primary "]/@href').extract_first()
+        absolute_next_url = response.urljoin(next_review_url)
+
+        
+        #yield Request(absolute_next_url, callback=self.parse_reviews)
+    
+    def parse(self, response):
+        #base_url = "www.tripadvisor.in"
+        main_div = response.xpath('//div[@id="taplc_hsx_hotel_list_lite_dusty_hotels_combined_sponsored_0"]')
+        div_list = main_div.xpath('//div[@class="meta_listing ui_columns large_thumbnail_mobile "]')
+        
+        for div in div_list:
+            hotel_url = response.urljoin(div.xpath('@data-url').extract_first())
+            #yield{"url":absolute_next_url}
+            yield Request(hotel_url, callback=self.parse_hotel)
+
+    
